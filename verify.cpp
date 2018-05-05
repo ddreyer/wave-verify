9e6889e3ae22659b108fdd5f8a3fc21c4d20f11b
Dylan <ddreyer@berkeley.edu>
2018-05-05 13:30:16 -0700

    fixed base64 decode

--- verify.cpp
+++ verify.cpp
@@ -2,6 +2,8 @@
 #include <streambuf>
 #include <iostream>
 #include <stdio.h>
+#include <algorithm>
+#include <string>
 
 #include "objects.h"
 #include "aes-gcm/gcm.h"
@@ -69,7 +71,7 @@ static int report_error(OssControl *ctl, const char *where, ASN1Exception &exc)
  * https://renenyffenegger.ch/notes/development/Base64/Encoding-and-decoding-base-64-with-cpp
 */
 static inline bool is_base64(unsigned char c) {
-  return (isalnum(c) || (c == '+') || (c == '/'));
+    return (isalnum(c) || (c == '+') || (c == '/'));
 }
 
 static const string base64_chars = 
@@ -79,44 +81,44 @@ static const string base64_chars =
 
 
 string base64_decode(string const& encoded_string) {
-  int in_len = encoded_string.size();
-  int i = 0;
-  int j = 0;
-  int in_ = 0;
-  unsigned char char_array_4[4], char_array_3[3];
-  string ret;
+    int in_len = encoded_string.size();
+    int i = 0;
+    int j = 0;
+    int in_ = 0;
+    unsigned char char_array_4[4], char_array_3[3];
+    string ret;
 
-  while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
-    char_array_4[i++] = encoded_string[in_]; in_++;
-    if (i ==4) {
-      for (i = 0; i <4; i++)
-        char_array_4[i] = base64_chars.find(char_array_4[i]);
+    while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
+        char_array_4[i++] = encoded_string[in_]; in_++;
+        if (i ==4) {
+        for (i = 0; i <4; i++)
+            char_array_4[i] = base64_chars.find(char_array_4[i]);
 
-      char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-      char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-      char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+        char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-      for (i = 0; (i < 3); i++)
-        ret += char_array_3[i];
-      i = 0;
+        for (i = 0; (i < 3); i++)
+            ret += char_array_3[i];
+        i = 0;
+        }
     }
-  }
 
-  if (i) {
-    for (j = i; j <4; j++)
-      char_array_4[j] = 0;
+    if (i) {
+        for (j = i; j <4; j++)
+        char_array_4[j] = 0;
 
-    for (j = 0; j <4; j++)
-      char_array_4[j] = base64_chars.find(char_array_4[j]);
+        for (j = 0; j <4; j++)
+        char_array_4[j] = base64_chars.find(char_array_4[j]);
 
-    char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-    char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-    char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+        char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-    for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
-  }
+        for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+    }
 
-  return ret;
+    return ret;
 }
 
 std::string string_to_hex(const std::string& input)
@@ -144,16 +146,12 @@ int main() {
     string pemStr((istreambuf_iterator<char>(t)),
                              istreambuf_iterator<char>());
 
+    // extract proof content from .pem file
     pemStr.erase(0, pemStr.find("\n") + 1);
     int idx = pemStr.find("-----END WAVE");
     pemStr.erase(idx, pemStr.find("\n", idx));
-
-    // TODO: base64_decode doesn't work right now
-    // string derEncodedData(base64_decode(pemStr));
-    
-    ifstream v("proof.der");
-    string derEncodedData((istreambuf_iterator<char>(v)),
-                             istreambuf_iterator<char>());
+    pemStr.erase(remove(pemStr.begin(), pemStr.end(), '\n'), pemStr.end());
+    string derEncodedData(base64_decode(pemStr));
 
     printf("Binary size: %lu\n", derEncodedData.length());
     if (derEncodedData.length() == 0) {
