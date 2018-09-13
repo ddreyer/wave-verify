40e3ec15dee362f84397946bb08533fd18975914
Dylan <ddreyer@berkeley.edu>
2018-09-12 22:17:43 -0700

    new asn1c library built

--- Makefile
+++ Makefile
@@ -1,42 +1,43 @@
-CC      = g++
-CFLAGS  = -I/usr/local/asn1cpp/include
-LIBS = -lm -lnsl -pthread
-OSSLIBS = /usr/local/asn1cpp/lib/libosscpp.so /usr/local/asn1cpp/lib/libcppsoed.so -ldl
-AES_OBJECTS=$(AES_SRCS:.c=.o) 
+CCX=g++
+CC=gcc
+CFLAGS=-I. -Iasn1c_files
+AES_OBJECTS=$(AES_SRCS:.c=.o)
 AES_SRCS=aes-gcm/aes.c aes-gcm/cipher.c aes-gcm/cipher_wrap.c aes-gcm/gcm.c aes-gcm/utils.c
-AESCFLAGS=-c -Wall
 ED_OBJECTS=$(ED_SRCS:.c=.o)
 ED_SRCS=$(wildcard ed25519/src/*.c)
 HASH_OBJECTS=$(HASH_SRCS:.cpp=.o)
 HASH_SRCS=hash-library/keccak.cpp
+ASN1_OBJECTS=$(ASN1_SRCS:.c=.o)
+ASN1_SRCS=$(wildcard asn1c_files/*.c)
+
 
 all: verify
 
-verify: $(HASH_OBJECTS) $(ED_OBJECTS) $(AES_OBJECTS) objects.o verify.o main.o
-		g++ -o $@ $^ $(LIBS) $(OSSLIBS)
+verify: $(HASH_OBJECTS) $(ED_OBJECTS) $(AES_OBJECTS) $(ASN1_OBJECTS) verify.o main.o
+		$(CCX) -o $@ $^
 		./verify
 
 verify.o: verify.cpp
-		$(CC) -I. $(CFLAGS) -c $<
-
-objects.o: objects.cpp
-		$(CC) -I. $(CFLAGS) -DOSSPRINT -c $<
+		$(CCX) $(CFLAGS) -c $<
 
 main.o: main.cpp
-		$(CC) -I. $(CFLAGS) -c $<
+		$(CCX) $(CFLAGS) -c $<
 
 $(AES_OBJECTS): aes-gcm/%.o : aes-gcm/%.c
-		gcc -c -Wall -c $< -o $@ 
+		$(CC) -c -Wall -c $< -o $@ 
 
 $(ED_OBJECTS): ed25519/src/%.o: ed25519/src/%.c
-		gcc -c $< -o $@
+		$(CC) -c $< -o $@
 
 $(HASH_OBJECTS): hash-library/%.o: hash-library/%.cpp
-		$(CC) -c $< -o $@
+		$(CCX) -c $< -o $@
+
+$(ASN1_OBJECTS): asn1c_files/%.o: asn1c_files/%.c
+		$(CC) $(CFLAGS) -c $< -o $@
 
 .PHONY: clean cleanest
 
 clean:
-		rm *.o
+		rm -f *.o aes-gcm/*.o ed25519/*.o hash-library/*.o asn1c_files/*.o
 
 		
--- verify.h
+++ verify.h
@@ -11,51 +11,46 @@
 #include <list>
 #include <unordered_map>
 #include <vector>
-#include "objects.h"
+
+#include <WaveEntity.h>
+#include <WaveAttestation.h>
+#include <AttestationVerifierBody.h>
+#include <RTreeStatement.h>
 #include "aes-gcm/gcm.h"
 #include "ed25519/src/ed25519.h"
 #include "hash-library/keccak.h"
 
 class EntityItem {
 private:
-    WaveEntity *entity;
+    WaveEntity_t *entity;
     std::string entityDer;
 public:
-    EntityItem(WaveEntity *entity, std::string entityDer);
-    WaveEntity * get_entity();
+    EntityItem(WaveEntity_t *entity, std::string entityDer);
+    WaveEntity_t * get_entity();
     std::string get_der();
 };
 
 class AttestationItem {
 private:
-    WaveAttestation *attestation;
-    AttestationVerifierBody decryptedBody;
+    WaveAttestation_t *attestation;
+    AttestationVerifierBody_t decryptedBody;
 public:
-    AttestationItem(WaveAttestation *att, AttestationVerifierBody dBody);
-    WaveAttestation * get_att();
-    AttestationVerifierBody get_body();
+    AttestationItem(WaveAttestation_t *att, AttestationVerifierBody_t dBody);
+    WaveAttestation_t * get_att();
+    AttestationVerifierBody_t get_body();
 };
 
-class RTreeStatementItem {
-private:
-    RTreeStatement::permissionSet permissionSet;
-    std::list<std::string> permissions;
-    std::string intersectionResource;
-public:
-    RTreeStatementItem(RTreeStatement::permissionSet pSet, std::list<std::string> perms, std::string iResource);
-    RTreeStatement::permissionSet get_permissionSet();
-    std::list<std::string> get_permissions();
-    std::string get_interResource();
-};
-
-class ASN1Exception {
-private:
-    int code;
-public:
-    ASN1Exception(int asn1_code);
-    ASN1Exception(const ASN1Exception & that);
-    int get_code() const;
-};
+// class RTreeStatementItem {
+// private:
+//     struct RTreeStatement_t rTreeStatement;
+//     std::list<std::string> permissions;
+//     std::string intersectionResource;
+// public:
+//     RTreeStatementItem(RTreeStatement::permissionSet pSet, std::list<std::string> perms, std::string iResource);
+//     RTreeStatement::permissionSet get_permissionSet();
+//     std::list<std::string> get_permissions();
+//     std::string get_interResource();
+// };
 
 int verify(std::string pemContent);
 
